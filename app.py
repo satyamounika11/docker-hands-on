f404cff3daeacb09219bf42a8f70bfa3b17a895b
satyamounika11 <37068004+satyamounika11@users.noreply.github.com>
2025-12-15 19:26:29 +0530

    multiple services

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "Hello from Docker! 🚀"
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+counter = redis.Redis(host="redis", port=6379)
 
 @app.route('/')
 def home():
-    return "Hello from Docker! 🚀"
+    count = counter.incr('hits')
+    return f"Page visited {count} times! 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
